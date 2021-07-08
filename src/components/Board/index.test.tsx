c0b6390295ec02a4b0799284ccd76cbd96886376
Naik <atm@Naiks-MacBook-Air.local>
2021-07-08 20:47:47 +0530

    ATM | add failing onclick test for board

--- src/components/Board/index.test.tsx
+++ src/components/Board/index.test.tsx
@@ -3,4 +3,10 @@ import { render } from '@testing-library/react';
 test('renders Board without crashing', () => {
     render(<Board values = {[null,null,null,null,null,null,null,null,null]}/>);
   });
+  test('calls onClick set by parent', () => {
+    const onClick = jest.fn();
+    const { getByTestId } = render(<Board values = {[null,null,null,null,null,null,null,null,null]} onClick={onClick}/>);
+    getByTestId('square').click()
+    expect(onClick).toBeCalledWith(0)
+});
   
--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,22 +1,22 @@
 import { XO } from "../Square";
 import Square from "../Square";
-const Board=({values}:{values:(XO|null)[]})=>{
+const Board=({values,onClick}:{values:(XO|null)[],onClick:(position:number)=>void})=>{
     return (
         <div>
             <div>
-                <Square position={0} xo={values[0]}/>
-                <Square position={1} xo={values[1]}/>
-                <Square position={2} xo={values[2]}/>
+                <Square position={0} xo={values[0]} />
+                <Square position={1} xo={values[1]} />
+                <Square position={2} xo={values[2]} />
             </div>
             <div>
-                <Square position={3} xo={values[3]}/>
-                <Square position={4} xo={values[4]}/>
-                <Square position={5} xo={values[5]}/>
+                <Square position={3} xo={values[3]} />
+                <Square position={4} xo={values[4]} />
+                <Square position={5} xo={values[5]} />
             </div>
             <div>
-                <Square position={6} xo={values[6]}/>
-                <Square position={7} xo={values[7]}/>
-                <Square position={8} xo={values[8]}/>
+                <Square position={6} xo={values[6]} />
+                <Square position={7} xo={values[7]} />
+                <Square position={8} xo={values[8]} />
             </div>
         </div>
     )
