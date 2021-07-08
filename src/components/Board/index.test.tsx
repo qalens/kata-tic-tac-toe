87e19271acb84bce9411c41d3aee0f6c01525093
Naik <atm@Naiks-MacBook-Air.local>
2021-07-08 20:35:44 +0530

    ATM | pass all test for display logic

--- src/components/Board/index.test.tsx
+++ src/components/Board/index.test.tsx
@@ -1,6 +1,6 @@
 import Board from '.'
 import { render } from '@testing-library/react';
 test('renders Board without crashing', () => {
-    render(<Board/>);
+    render(<Board values = {[null,null,null,null,null,null,null,null,null]}/>);
   });
   
--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import Board from "../Board";
 const Game = ()=>{
+    const [values,setValues] = useState([null,null,null,null,null,null,null,null,null])
     return (
-        <Board/>
+        <Board values={values}/>
     )
 }
 export default Game;
