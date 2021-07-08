5c34eff6fa6161e5020548445f7fe61f3d5f1468
Naik <atm@Naiks-MacBook-Air.local>
2021-07-08 20:43:14 +0530

    ATM | pass onclick test

--- src/components/Square/index.tsx
+++ src/components/Square/index.tsx
@@ -3,7 +3,7 @@ export enum XO{
     O='O'
 }
 const Square=({position,xo,onClick}:{position:number,xo:XO|null,onClick:(position:number)=>void})=>{
-    return <button data-testid="square">
+    return <button data-testid="square" onClick={()=>{onClick(position)}}>
         {xo==null?"":xo}
     </button>
 }
