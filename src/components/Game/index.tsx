afeefb3962a4d66311fd4d802d6e0585d24e6464
Naik <atm@Naiks-MacBook-Air.local>
2021-07-08 21:10:11 +0530

    ATM | added logic and passed test for taking turns

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -4,11 +4,16 @@ import {XO} from '../Square'
 const Game = ()=>{
     const initValues:(XO|null)[]=[null,null,null,null,null,null,null,null,null];
     const [values,setValues] = useState(initValues)
-    
+    const [turn,setTurn] = useState(XO.X);
     const handleClick=(position:number)=>{
         let new_values=[...values];
-        new_values[position] = XO.X;
+        new_values[position] = turn;
         setValues(new_values);
+        if(turn==XO.X){
+            setTurn(XO.O)
+        } else {
+            setTurn(XO.X)
+        }
     }
     return (
         <Board values={values} onClick={handleClick}/>
